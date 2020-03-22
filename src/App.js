4f68f86122afe7ce4e021097cd4f494f83ac4b72
sergey_spynu <sspynu@tsconsulting.com>
2020-03-22 18:43:19 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+	Container,
+	Row,
+	Col,
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	CardTitle,
+	CardSubtitle,
+	Button,
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.PureComponent {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			list: [], // список комиксов
+			watched: [], // список смотрел / не смотрел
+			errState: null, // состояние запроса - есть ошибка / все ок
+		}
+
+	}
+
+	componentDidMount() {
+		const movies = fetch('https://api.tvmaze.com/search/shows?q=batman');
+
+		movies.then((data) => {
+
+			return data.json();
+
+		}).then((data) => {
+
+			this.setState({
+				list: data || [],
+			});
+			console.log("DATA: ", data);
+
+		}).catch((e) => {
+			console.log("REQUEST ERROR: ". e);
+		});
+	}
+
+	renderCard = () => {
+		const item = this.state.list[0] ? this.state.list[0] : {};
+
+		const {
+			name = "",
+			url = "",
+			image = {},
+		} = item;
+
+		return (
+			<div>
+			<Card>
+				<CardImg top width="100%" src={ image.medium} alt={ name } />
+				<CardBody>
+					<CardTitle>{ name }</CardTitle>
+					<CardSubtitle>Card subtitle</CardSubtitle>
+					<CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+					<Button>Button</Button>
+				</CardBody>
+			</Card>
+			</div>
+		)
+	};
+
+
+	render() {
+
+		return (
+			<Container>
+				<Row>
+					<Col><h1>Batman Movies</h1></Col>
+				</Row>
+				<Row>
+					<Col>
+						{
+							this.renderCard()
+						}
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						{
+							JSON.stringify(this.state.list)
+						}
+					</Col>
+				</Row>
+			</Container>
+
+		);
+
+	}
+
 }
 
 export default App;
