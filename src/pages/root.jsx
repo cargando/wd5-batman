98ee8e06ac881e212a03bcd6422e2acaac30b7ca
sergey_spynu <sspynu@tsconsulting.com>
2020-03-31 19:46:28 +0300

    with table in description

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import * as PropTypes from 'prop-types';
 import { Col, Row } from "reactstrap";
 import MovieCard from "../components/MovieCard";
